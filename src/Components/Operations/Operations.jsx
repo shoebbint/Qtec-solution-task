3b1a429b7e6105259d88b3efd80ab2a76e3e3a06
shoebbint <shoebbint@gmail.com>
2024-02-13 01:41:59 +0600

    validated

---
+++ src/Components/Operations/Operations.jsx
@@ -0,0 +1,15 @@
+
+import Edit from "./Edit/Edit";
+
+const Operations = () => {
+   
+  return (
+    <div>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-outline btn-warning">Success</button>
+<Edit></Edit>
+    </div>
+  );
+};
+
+export default Operations;
--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Operations from "../Operations/Operations";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
@@ -9,55 +10,49 @@ const TaskList = () => {
 
   return (
     <div>
-      <div className=" sm:py-8  sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+      <div className=" sm:py-8 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+        <div className=" p-2">
+          <div
+            style={{ backgroundColor: "#C4C2E9" }}
+            className="flex flex-wrap  w-full card lg:card-side shadow-2xl "
+          >
+            <thead className="card-body">
+              <tr className="flex flex-col lg:flex-row justify-between text-bold text-xl">
+                <th>Name</th>
+                <th>Priority</th>
+                <th>Status</th>
+                <th>Operation</th>
+                <th></th>
+              </tr>
+            </thead>
+          </div>
+        </div>
         {tasks.map((task, index) => (
-          <div key={index} className="flex flex-wrap -m-2">
-            <div className="p-2  w-full">
-              <div className="card lg:card-side bg-base-100 shadow-xl">
-                <div className="card-body flex ">
-                  <tr className="flex flex-col lg:flex-row justify-between">
-                    <th>
-                      <label>
-                        <input type="checkbox" className="checkbox" />
-                      </label>
-                    </th>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src="/tailwind-css-component-profile-2@56w.png"
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
-                        <div>
-                          <div className="font-bold">Hart Hagerty</div>
-                          <div className="text-sm opacity-50">
-                            United States
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      Zemlak, Daniel and Leannon
-                      <br />
-                      <span className="badge badge-ghost badge-sm">
-                        Desktop Support Technician
-                      </span>
-                    </td>
-                    <td>Purple</td>
-                    <th>
-                      <button className="btn btn-ghost btn-xs">details</button>
-                    </th>
-                  </tr>
+          <div key={index} className=" p-2">
+            <div className="flex flex-wrap  w-full card lg:card-side bg-base-100 shadow-xl ">
+              <tr className="card-body flex flex-col lg:flex-row justify-between">
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div>
+                      <div className="font-bold">{task.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td className="badge badge-secondary badge-outline p-5">
+                  {task.priority}
+                  <br />
+                </td>
+                <td>Purple</td>
+                <td><Operations></Operations></td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
 
-                  {/* <div className="card-actions lg:justify-end justify-center">
+              {/* <div className="card-actions lg:justify-end justify-center">
                     {" "}
                     <button className="btn btn-primary">Listen</button>
                   </div> */}
-                </div>
-              </div>
             </div>
           </div>
         ))}
